6010999e1348090ed8f3393b36d56a65d8ace613
Jeroen Haegebaert <jeroen.haegebaert@outlook.be>
2020-12-31 13:32:23 +0100

    hello world

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # pi-top-samples
 samples for .net core development on the pi-top
+
+## prerequisites
+To run these samples, you need a pi-top (https://www.pi-top.com) and for most  samples also their foundation kit.
+
+The samples assume that you have set up the pi-top, and can connect to it over ssh.
+
+I'm using the (sshdeploy)[https://github.com/unosquare/sshdeploy] tool to deploy code to the pi. 
+
+The workflow to push code to the pi-top is then:
+
+```
+dotnet build -c release
+dotnet sshdeploy push -w <pi-top password>
+```
+
+To run the sample, connect to your pi-top via ssh (`ssh pi@pi-top`), and run the following commands (example for the helloworld sample; similar for the others):
+
+```
+cd helloworld
+dotnet helloworld.dll
+```
---
+++ helloworld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using PiTop;
+using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+
+var module = PiTop4Board.Instance;
+var display = module.Display;
+
+var message = "Hello from .NET 5";
+
+var font = SystemFonts.Collection.Find("Roboto").CreateFont(16);
+
+module.Display.Draw((context,cr) => {
+                context.Clear(Color.Black);
+                var rect = TextMeasurer.Measure(message, new RendererOptions(font));
+                var x = (cr.Width - rect.Width) / 2;
+                var y = (cr.Height + rect.Height) / 2;
+                context.DrawText(message, font, Color.White, new PointF(0, 0));
+            });
+
+Console.WriteLine("Press enter to exit");
+Console.ReadLine();
